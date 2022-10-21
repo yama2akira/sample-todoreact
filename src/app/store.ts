6ddc1203cd6e53c62938380f71bf54c57ba5442f
Akira Yamamoto <yama2akira@gmail.com>
2022-10-21 16:19:58 +0900

    init

---
+++ src/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import taskReducer from '../slices/todoSlice';
+
+export const store = configureStore({
+  reducer: {
+    task: taskReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
