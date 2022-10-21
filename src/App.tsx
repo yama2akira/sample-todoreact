f91b88acdb8d8d8501d90a34d05198bb3fe535e9
Akira Yamamoto <yama2akira@gmail.com>
2022-10-21 17:11:20 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { MainTask } from './components/MainTask';
+import { MainTag } from './components/MainTag';
 //import { MainTask } from './components/MainTask';
 //import { MainTag } from './components/MainTag';
 
@@ -20,8 +22,8 @@ const App: FC = () => {
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
-            {/* <Route path="/" element={<MainTask />} />
-            <Route path="/tags" element={<MainTag />} /> */}
+            <Route path="/" element={<MainTask />} />
+            <Route path="/tags" element={<MainTag />} />
           </Routes>
         </BrowserRouter>
         <ReactQueryDevtools initialIsOpen={false} />
